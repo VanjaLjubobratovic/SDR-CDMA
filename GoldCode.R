f7909f674d0b76aa041acb0363bbb0f3c0b5fb03
Vanja Ljubobratovic <vanja1708@gmail.com>
2023-05-30 15:36:33 +0200

    First Commit

---
+++ GoldCode.R
@@ -0,0 +1,58 @@
+rm(list=ls()) 
+
+
+int_xor <- function(bit1, bit2) {
+  return (abs(bit1 - bit2))
+}
+
+generate_gold_code <- function(seed){
+  # Generates codes up to 127 bits
+  g1 <- c(1, 1, 1, 0, 0, 0, 1)
+  g2 <- c(0, 0, 1, 0, 0, 0, 1)
+  last <- length(g1)
+  
+  # Bit number 1 is left, bit number 7 is right
+  r1 <- c(0, 0, 0, 0, 0, 0, 1)
+  r2 <- seed
+  
+  gold_code <- c()
+  
+  for(i in 1:20) {
+    # Compute XOR of last bits in shift registers and output it
+    output_bit <- int_xor(r1[last], r2[last])
+    
+    in_r1 = NA
+    in_r2 = NA
+    
+    # Calculate new input bits via prefered pairs
+    for(j in seq(last, 1, -1)) {
+      if(g1[j] == 1) {
+        if(is.na(in_r1)) {
+          in_r1 <- r1[j]
+        } else {
+          in_r1 <- int_xor(in_r1, r1[j])
+        }
+      }
+      
+      if(g2[j] == 1) {
+        if(is.na(in_r2)) {
+          in_r2 <- r2[j]
+        } else {
+          print(in_r2)
+          in_r2 <- int_xor(in_r2, r2[j])
+        }
+      }
+    }
+    
+    # Shift registers
+    r1 <- c(in_r1, r1[1:last-1])
+    r2 <- c(in_r2, r2[1:last-1])
+    
+    # Store new output
+    gold_code <- c(gold_code, output_bit)
+  }
+  
+  return (gold_code)
+}
+
+
